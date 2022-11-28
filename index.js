caf0cab687e1cbc5a1107d43d47272d924edf920
Jackline <jackline.muthama@student.moringaschool.com>
2022-11-28 18:42:20 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here
+function findMatching(drivers,name) {
+  return drivers.filter(function (driver){
+    if (driver.toUpperCase()===name.toUpperCase()){
+      return true
+    }
+  })
+}
+function fuzzyMatch(drivers,pattern) {
+  return drivers.filter(function (driver){
+    let firstTwoCharacters= driver.slice(0,pattern.length)
+    if (firstTwoCharacters===pattern) {
+      return true
+    }
+  })
+}
+function matchName(drivers,name) {
+  return drivers.filter(function(driver){
+    let nameOfDriver=driver.name
+    if (nameOfDriver===name){
+      return true
+    }
+  })
+}
